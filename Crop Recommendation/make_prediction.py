551ca34f78aadf935858437d20cf231ead0f1454
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2021-06-19 15:03:51 +0530

    Added predictions file

---
+++ Crop Recommendation/make_prediction.py
@@ -0,0 +1,23 @@
+import pandas as pd 
+import numpy as np
+import pickle 
+
+filename = "crop_model.sav"
+
+# Load model
+loaded_model = pickle.load(open(filename, 'rb'))
+
+# Take inputs
+colm_names = ["N","P","K","temperature","humidity","ph","rainfall"]
+d = {}
+for i in range(len(colm_names)):
+    print("Enter the value for " + colm_names[i])
+    ipt = float(input())
+    d[colm_names[i]] = ipt 
+
+# Convert to Series object for prediction
+x = pd.Series(d)
+x = x.values.reshape(1, -1)
+
+# Make prediction 
+print(loaded_model.predict(x))
